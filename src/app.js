0b7d6c3def9e7f7c342e8531283a0424b584e348
preet.crawl <preetbhingradiya.crawlapps@gmail.com>
2024-08-21 14:22:24 +0530

    deploye

--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended:true }))
+
+let user = [
+    {name: "John Doe", email: "johndoe@example.com"},
+]
+
 app.get("/test", (req,res)=>{
     res.status(200).send("Test node emdpoint is work")
+})
+
+app.post('/new/user',(req,res)=>{
+    let {name, email} = req.body
+
+    if(!name || !email) return res.status(404).send("Enter name or email")
+
+    user.push({name, email})
+    res.status(201).send(user)
+
+})
+
+app.get('/user', (req,res) => {
+   res.status(200).send(user)
 })
 
 module.exports = app
--- src/app.test.js
+++ src/app.test.js
@@ -8,4 +8,16 @@ describe('tesst endpoint', () => {
         expect(response.status).toBe(200);
         expect(response.text).toBe('Test node emdpoint is work');
     });
+
+    it('should return error message when name or email is missing', async() => {
+        const response = await requset.post('/new/user').send({})
+        expect(response.status).toBe(404);
+        expect(response.text).toBe('Enter name or email');
+    });
+
+    it('should return a list of users', async() => {
+        const response = await requset.get('/user')
+        expect(response.status).toBe(200);
+        expect(Array.isArray(response.body)).toBe(true);
+    });
 });
